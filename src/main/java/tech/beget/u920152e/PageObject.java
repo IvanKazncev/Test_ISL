4f80fb4e7db3b296e7358cbf57aedc4506a922e1
IvanKazncev <vowkaz@inbox.ru>
2023-02-04 23:04:04 +0400

    Done

---
+++ src/main/java/tech/beget/u920152e/PageObject.java
@@ -0,0 +1,26 @@
+package tech.beget.u920152e;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public record PageObject(WebDriver webDriver) {
+     public static final String StartURL = "http://u920152e.beget.tech/";
+     public static final String Avitorization = "form_auth_block_head_text";
+     public static final String Email = "auth_email";
+     public static final String Password = "auth_pass";
+     public static final String Submit_Button = "form_auth_button";
+     public static final String Age = "//P[text()='Сколько Вам лет?']";
+
+
+
+
+
+     public void Start(){
+         webDriver.get(StartURL);
+     }
+
+}
---
+++ src/main/java/tech/beget/u920152e/Tests.java
@@ -0,0 +1,62 @@
+package tech.beget.u920152e;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Tests {
+    public static final WebDriver webDriver;
+    public static final PageObject PAGE_OBJECT;
+    public static WebDriverWait wait;
+
+    //Процесс инициализации необходимых ресурсов
+    static {
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().window().maximize();
+        PAGE_OBJECT = new PageObject(webDriver);
+
+    }
+
+    @Test
+    public void TestSmoke() {
+        PAGE_OBJECT.Start();
+        Assert.assertTrue(PAGE_OBJECT.webDriver().findElement(By.className(PageObject.Avitorization)).isDisplayed());
+
+    }
+    @Test
+    public void TestSmoke2(){
+        PAGE_OBJECT.Start();
+        webDriver.findElement(By.name(PageObject.Email)).sendKeys("vowkaz@inbox.ru");
+        webDriver.findElement(By.name(PageObject.Password)).sendKeys("Test3123");
+        webDriver.findElement(By.className(PageObject.Submit_Button)).click();
+        String text = webDriver.findElement(By.xpath(PageObject.Age)).getText();
+        Assert.assertEquals(text,"Сколько Вам лет?");
+
+    }
+    @Test
+    public void MailInputTest(){
+        PAGE_OBJECT.Start();
+        webDriver.findElement(By.name(PageObject.Email)).sendKeys("vowkazinbox.ru");
+        webDriver.findElement(By.name(PageObject.Password)).sendKeys("Test3123");
+        webDriver.findElement(By.className(PageObject.Submit_Button)).click();
+        JavascriptExecutor js = (JavascriptExecutor)webDriver;
+        WebElement field = webDriver.findElement(By.name(PageObject.Email));
+        Boolean is_valid = (Boolean)js.executeScript("return arguments[0].checkValidity();", field);
+        Assert.assertFalse(is_valid);
+        }
+    }
+
+
+
+
